eb511276255c9af7c74fbea39a060952019e55ad
ggxsam <grgsamira10@gmail.com>
2025-02-10 10:31:24 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,442 @@
+
+// question no 1
+// let x=19
+// if(x>=18){
+//   console.log(`${x} is an adult`);
+// }else{
+//   console.log(`${x} is not an adult`)
+// }
+
+// question no2
+// let x=2
+// if(x>=0){
+//   console.log(`${x} is positive`);
+// }
+// else{
+//   console.log(`${x} is negative`);
+// }
+
+// question no 3
+// let x=2
+// if(x%2==0){
+//   console.log(`${x} is even`);
+// }
+// else{
+//   console.log(`${x}is odd`);
+// }
+
+// question no 4
+// let x='b'
+// if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+//   console.log(`${x} is a vowel`);
+// }
+// else{
+//   console.log(`${x} is a consonant`);
+// }
+
+// question no 5
+// let x=1
+// if(x==='0'){
+//   console.log(`${x} is zero`);
+// }else{
+//   console.log(`${x} is not zero`);
+// }
+
+// question no 6
+// let x=2
+// if(x%5==0){
+//   console.log(`${x} is divisible by 5`);
+// }else{
+//   console.log(`${x} is not divisible by 5`);
+// }
+
+// question no 7
+// let x=9
+// if(x>10){
+//   console.log(`${x} is greater than 10`);
+// }else if(x==10){
+//   console.log(`${x} is equal to 10`)
+//   }
+//   else{
+//   console.log(`${x} is less than 10`);
+// }
+
+// question no 8  
+// let x=2, y='2'
+// if(x===y){
+//   console.log(`${x} is equal to ${y}`);
+// }else{
+//   console.log(`${x} is not equal to ${y}`);
+// } 
+
+// question no 9  
+// let x='samira'
+// if(x.length>5){
+//   console.log(`${x} is a long string`);
+// }else{
+//   console.log(`${x} is a short string`);
+// }
+
+// question no 10
+// let x = 'hello'
+// if (x >= 'A' && x <= 'Z') {
+//   console.log(`${x} is an uppercase letter.`);
+// } else {
+//   console.log(`${x} is not an uppercase letter.`);
+// }
+
+// question no 11
+// let x = 'Monday'
+// if(x==='Saturday'){
+//   console.log(`${x} is weekend`);
+// }else{
+//   console.log(`${x} is not weekend`);
+// }
+
+// question no 12
+// let x = 11
+// if(x<12){
+//   console.log(`${x} is a child`);
+
+// }else{
+//   console.log(`${x} is not a child`);
+// }
+
+// question no 13
+// let temperature=30
+// if(temperature>30){
+//   console.log(`${temperature} is hot`);
+
+// }else{
+//   console.log(`${temperature} is not hot`);
+// }
+
+// question no 14
+// let x=10
+// if(x>1 && x<100){
+//   console.log(`${x} is between 1 and 100`);
+// }else{
+//   console.log(`${x} is not between 1 and 100`);
+// }
+
+// question no 15
+// let student=55
+// if(student>=50){
+//   console.log(`Student passed with ${student} marks`);
+// }else{
+//   console.log(`Student failed with ${student} marks`);
+// }
+
+// question no 16
+// let x=9
+// if(x%3==0){
+//   console.log(`${x} is divisible by 3`);
+// }else{
+//   console.log(`${x} is not divisible by 3`);
+// }
+
+// question no 17
+// let time=14
+// if(time<12){
+//   console.log(`It is ${time} AM So It is morning`);
+// }else{
+//   console.log(`It is ${time} PM So It is afternoon`);
+// }
+
+// question no 18
+// let age=19
+// if(age>13 && age<20){
+//   console.log(`Age ${age} is teenage`);
+// }else{
+//   console.log(`Age ${age} is not teenage`);
+// }
+
+// question no 19
+// let str='samira'
+// if(str.includes('a')){
+//   console.log(`${str} contains a `);
+// }else{
+//   console.log(` ${str} does not contain a`);
+// }
+
+// question no 20
+// let year=2000
+// if(year%4==0 && year%100!==0 || year%400==0){
+//   console.log(`${year} is a leap year`);
+// }else{
+//   console.log(`${year} is not a leap year`);
+// }
+
+// question no 21
+// let x=-1
+// if(x<0){
+//   console.log(`${x} is negative`);
+// }else{
+//   console.log(`${x} is positive`);
+// }
+
+// question no 22
+// let password="23434"
+// if(password=="1234"){
+//   console.log(`Access Granted`);
+// }else{
+//   console.log(`Access Denied`);
+// }
+
+// question no 23
+// let x=2, y=4
+// if(x>y){
+//   console.log(`${x} is greater than ${y}`);
+// }else{  
+//   console.log(`${x} is less than ${y}`);
+// }
+
+// question no 24
+// let x=0
+// let isPrime=true;
+
+// if(x<=1){
+//   isPrime=false;
+// }
+// else{
+//   for(let i=2; i<x;i++){
+//     if(x%i==0){
+//       isPrime= false;
+//       break;
+//     }
+//   }
+// }
+
+// if(isPrime){
+//   console.log(`${x} is a prime number`);
+// }else{
+//   console.log(`${x} is not a prime number`);
+// }
+
+// question no 25
+// let x=60
+// if(x>65){
+//   console.log(`${x} is eligible for senior citizen discount`);
+// }else{
+//   console.log(`${x} is not eligible for senior citizen discount`);
+// }
+
+// question no 26
+// let str1 = "hello";
+// let str2 = "hello";
+// if (str1 === str2) {
+//   console.log(`"${str1}" and "${str2}" Match`);
+// } else {
+//   console.log(`"${str1}" and "${str2}" Do not match`);
+// }
+
+// question no 27
+// let x=12
+// if(x%2==0){
+//   console.log(`${x} is multiple of 2`);
+// }else{
+//   console.log(`${x} is not multiple of 2`);
+// }
+
+// question no 28
+// let x=18
+// if(x>=18){
+//   console.log(`${x} is eligible to get driving license`);
+// }
+// else{
+//   console.log(`${x} is not eligible to get driving license`);
+// }
+
+// question no 29
+// let letter = "bchah";
+// if(letter>='a'&& letter<='z'){
+//   console.log(`${letter} is a lowercase letter`);
+// }
+// else{
+//   console.log(`${letter} is not a lowercase letter`);
+// }
+
+// question no 30
+// let x=20
+// if(x>=1 && x<=9){
+//   console.log(`${x} is a single digit number`);
+// }else{
+//   console.log(`${x} is not a single digit number`);
+// }
+
+// question no 31
+// let x=20
+// if(x%10==0){
+//   console.log(`${x} is divisible by 10`);
+// }
+// else{
+//   console.log(`${x} is not divisible by 10`);
+// }
+
+// question no 32
+// let month=7
+// if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
+//   console.log(`${month} has 31 days`);
+// }else{
+//   console.log(`${month} has 30 days`);
+// }
+
+// 
+
+// question no 33
+// let password= "sam@12232"
+// if(password.length>=8){
+//   console.log(`${password} is valid`);
+// }
+// else{
+//   console.log(`${password} is too short`);
+// }
+
+// question no 34
+// let number= 0
+// if(number===0){
+//   console.log(`${number} is zero`);
+// }else{
+//   console.log(`${number} is not zero`);
+// }
+
+// question no 35
+// let score=98
+// if(score>=90){
+//   console.log(`${score} is Grade A`);
+// }
+// else{
+//   console.log(`${score} is less than Grade A`);
+// }
+
+// question no 36
+// let str= ''
+// if(str.length==0){
+//   console.log(`${str} is empty`);
+// }else{
+//   console.log(`${str} is not empty`);
+// }
+
+// question no 37
+// let x=10
+// if(x%4==0){
+//   console.log(`${x} is multiple of 4`);  
+// }
+// else{
+//   console.log(`${x} is not multiple of 4`);
+// }
+
+// question no 38
+// let day='Sunday'
+// if(day=== 'Sunday'){
+//   console.log(`${day} is sunday`);
+// }else{
+//   console.log(`${day} is not sunday`);
+// }
+
+// question no 39
+// let str='Apple'
+// if(str[0]==='A'){
+//   console.log(`${str} starts with A`);
+// }else{
+//   console.log(`${str} does not start with A`);
+// }
+
+// question no 40
+// let str='helloworld'
+// if(str.includes(' ')){
+//   console.log(`${str} contains space`);
+// }else{
+//   console.log(`${str} does not contain space`);
+// }
+
+// question no 41
+// let str='hello'
+// if(str.length%2==0){
+//   console.log(`${str} is even`);
+// }else{
+//   console.log(`${str} is odd`);
+// }
+
+
+// question no 42
+// let temperature=100
+// if(temperature>100){
+//   console.log(`${temperature} is above 100 degrees`);
+// }else{
+//   console.log(`${temperature} is below 100 degrees`);
+// }
+
+
+// question no 43
+
+// let currentTime = new Date().getHours()
+// let hour = 6
+// if(hour>=9 && hour<=17){
+
+//   console.log(` It is work hour`);
+// }else{
+//   console.log(` It is not work hour`);
+// }
+
+// question no 44
+//check if a person’s Body Mass Index (BMI) is in the  healthy range (18.5 to 24.9). Print "Healthy" if true, otherwise "Not healthy
+// let weight = 60
+// let height = 1.5
+// let bmi = weight/(height*height) 
+//                 let bmi=20
+// if(bmi>=18.5 && bmi<=24.9){
+//   console.log(`Healthy`);
+// }else{
+//   console.log(`Not Healthy`);
+// }
+
+// question no 45
+// let x=3
+// if(x%2!==0){
+//   console.log(`${x} is odd`);
+// }else{
+//   console.log(`${x} is even`);
+// }
+
+// question no 46
+// let digit=10
+// if(digit>=0 && digit<=9){
+//   console.log(`${digit} is a digit`);
+// }
+// else{
+//   console.log(`${digit} is not a digit`);
+// }
+
+// question no 47
+// let str='hello!'
+// if(str.endsWith('!')){
+//   console.log(`Exclamation mark`);
+// }else{
+//   console.log(`No exclamation mark`);
+// }
+
+// question no 48
+// let day = new Date().getDay()
+// if(day>=1 && day<=5){
+//   console.log(`Weekday`);
+// }else{
+//   console.log(`Weekend`);
+// }
+
+// question no 49
+// let num = 16;
+// if (Math.sqrt(num) % 1 === 0) {
+//   console.log(`${num} is a perfect square`);
+// } else {
+//   console.log(`${num} is not a perfect square`);
+// }
+
+// question no 50
+// let temperature = 20
+// if(temperature>=15 && temperature<=25){
+//   console.log(`Comfortable`);
+// }else{
+//   console.log(`Uncomfortable`);
+// }
+
